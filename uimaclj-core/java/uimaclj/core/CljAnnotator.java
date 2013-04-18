9a009c4fef0c9fe718fd07b6e55904a8572f1572
Jens Haase <je.haase@googlemail.com>
2013-04-18 16:14:55 +0200

    Start removing the helper lib

--- uimaclj-core/java/uimaclj/core/CljAnnotator.java
+++ uimaclj-core/java/uimaclj/core/CljAnnotator.java
@@ -9,6 +9,10 @@ import org.apache.uima.analysis_engine.AnalysisEngine;
 import org.apache.uima.UimaContext;
 import org.apache.uima.resource.ResourceInitializationException;
 import uimaclj.helper.cljhelper;
+import clojure.lang.RT;
+import clojure.lang.Var;
+import clojure.lang.Compiler;
+import java.io.StringReader;
 
 public class CljAnnotator extends JCasAnnotator_ImplBase {
 
@@ -22,16 +26,26 @@ public class CljAnnotator extends JCasAnnotator_ImplBase {
 
   private UimaContext context;
 
+  private String clj = "(ns user)" + 
+    "(defn call [ns-str fn-str uima-context jcas]" +
+    "  (let [f (ns-resolve (symbol ns-str) (symbol fn-str))]" +
+    "    (f uima-context jcas)))";
+
+  Var callClj = null;
+
   @Override
   public void initialize(final UimaContext context) throws ResourceInitializationException {
     super.initialize(context);
     this.context = context;
+
+    Compiler.load(new StringReader(clj));
+    this.callClj = RT.var("user", "call");
   }
 
   @Override
   public void process(JCas jcas) throws AnalysisEngineProcessException {
     try {
-      cljhelper.call(ns, fn, context, jcas);
+      callClj.invoke(ns, fn, context, jcas);
     } catch (Exception e) {
       throw new AnalysisEngineProcessException(e);
     }
