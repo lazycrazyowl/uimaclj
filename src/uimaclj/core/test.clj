084489314a349f59206c6761e4368a3ad4751969
Jens Haase <je.haase@googlemail.com>
2013-04-19 17:17:12 +0200

    clean up the code

--- src/uimaclj/core/test.clj
+++ src/uimaclj/core/test.clj
@@ -14,22 +14,29 @@
        (partition 2)
        (mapcat (fn [[k v]] [(str k) (str v)]))))
 
-(defn create-primitive-description [f & params]
-  (let [desc (AnalysisEngineFactory/createPrimitiveDescription
-               CljAnnotator (to-array (apply uima-params params)))]
-    (ExternalResourceFactory/bindResource
-      desc CljAnnotator/PARAM_CLOJURE_RESOURCE ClojureResourceProvider
-      (into-array String [ClojureResourceProvider/PARAM_CLOJURE_FN (.getName (type f))]))
-    desc))
+(defn clj-resource-description [f]
+  (ExternalResourceFactory/createExternalResourceDescription
+    ClojureResourceProvider
+    (into-array
+      String
+      [ClojureResourceProvider/PARAM_CLOJURE_FN (.getName (type f))])))
+
+(defn clj-annotator [f & params]
+  (AnalysisEngineFactory/createPrimitive
+    CljAnnotator
+    (to-array
+      (concat
+        [CljAnnotator/PARAM_CLOJURE_RESOURCE (clj-resource-description f)]
+        (apply uima-params params)))))
 
 ;; How to run pipeline
 ;; The CljAnnotator gets two parameter the namespace and the
 ;; function name you want to call.
 (defn -main []
-  (let [jcas (JCasFactory/createJCas)
-        desc (create-primitive-description
-               (fn [context jcas] (println "hello world"))
-               :param1 "value1" :param2 "value2")
-        ae (AnalysisEngineFactory/createAggregate desc)]
+  (let [a 1
+        jcas (JCasFactory/createJCas)
+        ae (clj-annotator
+             (fn [context jcas] (println "hello world"))
+             :param1 "value1" :param2 "value2")]
     (.process ae jcas)
     nil))
